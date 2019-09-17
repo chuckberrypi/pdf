527edafbd71ce5c57017722cf0b5d7c1595f7843
David Stearns <d.f.stearns@gmail.com>
2019-09-16 18:17:57 -0700

    added filter to table of files.

--- ui_src/ui/core.cljs
+++ ui_src/ui/core.cljs
@@ -1,19 +1,25 @@
 (ns ui.core
+  (:import goog.string)
   (:require [reagent.core :as reagent :refer [atom]]
-            [clojure.string :as string :refer [split-lines]]))
+            [clojure.string :as string :refer [split-lines]]
+            [cljs-uuid-utils.core :as uuid])
+  )
 
 (def join-lines (partial string/join "\n"))
 
 (enable-console-print!)
 
-(defonce state        (atom 0))
+(defonce state (atom 0))
 (defonce shell-result (atom ""))
-(defonce command      (atom ""))
+(defonce command (atom ""))
 
 (defonce proc (js/require "child_process"))
 
 (defn append-to-out [out]
   (swap! shell-result str out))
+
+(def files (reagent/atom {}))
+(def monitor (reagent/atom ""))
 
 (defn run-process []
   (when-not (empty? @command)
@@ -27,16 +33,63 @@
       (.on (.-stdout p) "data" append-to-out))
     (reset! command "")))
 
+
+(defn dateString [date]
+           (str (goog.string/format "%02s" (+ 1 (.getMonth date))) "/" (.getDate date) "/" (.getFullYear date) " "
+                (.getHours date) ":" (.getMinutes date) ":" (.getSeconds date)))
+
+(defn filtered-state [st]
+  (filter
+    #(goog.string/contains
+       (string/upper-case (:file-name %))
+       (string/upper-case st))
+    (vals @files)))
+
+(defn filtered-table [st]
+  (if (< 0 (count @files))
+    [:table.table
+     [:tr
+      [:th.tableHeader  "File Name"]
+      [:th.tableHeader "Last Modified"]
+      [:th.tableHeader "UUID"]
+      [:th ""]]
+     (for [{:keys [file-name last-modified uid]} (filtered-state st)]
+       ^{:key uid} [:tr {:key uid}
+        [:td.dataCell file-name]
+        [:td.dataCell (dateString (js/Date. last-modified))]
+        [:td.dataCell (str uid)]
+        [:td [:button {:on-click #(swap! files dissoc uid)} "DELETE"]]])
+     ]))
+
+(defn file-table []
+  (let [filter-pred (reagent/atom "")]
+    (fn []
+      [:div
+       [:div "Filter: " [:input {:type "text"
+                                 :on-change
+                                       (fn [e] (reset! filter-pred (.-value (.-target e))))}]]
+      [filtered-table @filter-pred]])))
+
+
+(defn file-picker []
+  [:input {:type "file" :multiple true
+           :on-change (fn [e]
+                        (let [file-list (.-files (.-currentTarget e))
+                              list-len (.-length file-list)]
+                          (doseq [i (range list-len)]
+                            (let [file (.item file-list i)
+                                  uid (uuid/make-random-uuid)]
+                              (swap! files assoc uid {:file-name (.-name file)
+                                                      :last-modified (.-lastModified file)
+                                                      :uid uid})
+
+                              )
+                            ) (js/alert @files)))}])
+
 (defn root-component []
   [:div
-   [:div.logos
-    [:img.electron {:src "img/electron-logo.png"}]
-    [:img.cljs {:src "img/cljs-logo.svg"}]
-    [:img.reagent {:src "img/reagent-logo.png"}]]
-   [:pre "Versions:"
-    [:p (str "Node     " js/process.version)]
-    [:p (str "Electron " ((js->clj js/process.versions) "electron"))]
-    [:p (str "Chromium " ((js->clj js/process.versions) "chrome"))]]
+   [file-picker]
+   [file-table]
    [:button
     {:on-click #(swap! state inc)}
     (str "Clicked " @state " times")]
@@ -46,11 +99,11 @@
                    (.preventDefault e)
                    (run-process))}
      [:input#command
-      {:type :text
-       :on-change (fn [^js/Event e]
-                    (reset! command
-                            ^js/String (.-value (.-target e))))
-       :value @command
+      {:type        :text
+       :on-change   (fn [^js/Event e]
+                      (reset! command
+                              ^js/String (.-value (.-target e))))
+       :value       @command
        :placeholder "type in shell command"}]]]
    [:pre (join-lines (take 100 (reverse (split-lines @shell-result))))]])
 
